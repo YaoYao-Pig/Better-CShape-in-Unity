be1b60ef5fc644183ea18d65ba69b7cc17596793
摇摇猪 <73538420+YaoYao-Pig@users.noreply.github.com>
2024-12-16 16:50:11 +0800

    Add Better.Algorithm.HeapSort

---
+++ code/Algorithm/HeapSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better
+{
+    public partial class Algorithm
+    {
+        public static List<T> HeapSort<T>(List<T> list,Functor.Comparetor<T> comparetor)
+        {
+            Container.Priority_Queue<T> priority_Queue = new Container.Priority_Queue<T>(comparetor);
+            priority_Queue.InserArray(list);
+
+            List<T> result=new List<T>();
+            while (!priority_Queue.IsEmpty())
+            {
+                result.Add(priority_Queue.Pop());
+            }
+            return result;
+        }
+    }
+}
